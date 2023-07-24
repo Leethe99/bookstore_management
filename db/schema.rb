efc7294d0aa175ae405e905df9eeed5db0702112
Rafael Lee <leethe99@gmail.com>
2023-07-24 12:10:26 -0600

    migrations and seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_21_001215) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -20,8 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_001215) do
   end
 
   create_table "inventories", force: :cascade do |t|
-    t.integer "store_id", null: false
-    t.integer "book_id", null: false
+    t.bigint "store_id", null: false
+    t.bigint "book_id", null: false
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
